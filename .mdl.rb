1facf12ae9b55f6f5b796def1056de99fa6a6b2f
Raoul Scarazzini <rasca@mmul.it>
2023-02-09 17:33:42 +0100

    Make mdl rules more reliable
    There are some markdown linter rules that are not necessary with
    Marp. This commit explicits them inside .mdl.rb.

--- .mdl.rb
+++ .mdl.rb
@@ -1,3 +1,10 @@
 all
-rule 'MD013', :code_blocks => false, :ignore_code_blocks => true, :tables => false
+# Code blocks and tables are not meant to be just 80 chars long
+# https://github.com/markdownlint/markdownlint/blob/main/docs/RULES.md#md013---line-length
+rule 'MD013', :ignore_code_blocks => true, :tables => false
+# There is no way to make this work, somehow
+# https://github.com/markdownlint/markdownlint/blob/main/docs/RULES.md#md007---unordered-list-indentation
+exclude_rule 'MD007'
+# Marp wants html in some ways, so this rule should be omitted
+# https://github.com/markdownlint/markdownlint/blob/main/docs/RULES.md#md007---unordered-list-indentation
 exclude_rule 'MD033'
