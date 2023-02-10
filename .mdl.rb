2345ae670947c6e3f33867b74d2be1dff215fabe
Raoul Scarazzini <rasca@mmul.it>
2023-02-10 17:15:44 +0100

    Fix ordered list item prefix for Markdown linter
    Without this commit, markdown lists like:
    
    1.
    2.
    3.
    
    would be considered an error.

--- .mdl.rb
+++ .mdl.rb
@@ -5,6 +5,9 @@ rule 'MD013', :ignore_code_blocks => true, :tables => false
 # There is no way to make this work, somehow
 # https://github.com/markdownlint/markdownlint/blob/main/docs/RULES.md#md007---unordered-list-indentation
 exclude_rule 'MD007'
+# We want ordered list with style 'ordered'
+# https://github.com/markdownlint/markdownlint/blob/main/docs/RULES.md#md029---ordered-list-item-prefix
+rule 'MD029', :style => :ordered
 # Marp wants html in some ways, so this rule should be omitted
 # https://github.com/markdownlint/markdownlint/blob/main/docs/RULES.md#md007---unordered-list-indentation
 exclude_rule 'MD033'
